109fa7a1c2f412940302d4c885320453755813c2
pku7yang <pku7yang@gmail.com>
2019-04-05 13:07:31 +0100

    load_pretrained_bert

--- src/train.py
+++ src/train.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
     parser.add_argument("-model_path", default='../models/')
     parser.add_argument("-result_path", default='../results/cnndm')
     parser.add_argument("-temp_dir", default='../temp')
-    parser.add_argument("-bert_config_path", default='../bert_config_base.json')
+    parser.add_argument("-bert_config_path", default='../bert_config_uncased_base.json')
 
     parser.add_argument("-batch_size", default=1000, type=int)
 
